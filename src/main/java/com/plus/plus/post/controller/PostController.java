399e7ea38d44aeeedbfdb547ef813b8d2c0c5fc5
oneseel <seno1006@gmail.com>
2023-12-15 15:00:36 +0900

    feat : 게시글 삭제 API

--- src/main/java/com/plus/plus/post/controller/PostController.java
+++ src/main/java/com/plus/plus/post/controller/PostController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -79,5 +80,20 @@ public class PostController {
   }
 
   // 게시글 삭제
+  @DeleteMapping("{postId}")
+  public ResponseEntity<?> deletePost(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long postId) {
 
+    User loginUser = userDetails.getUser();
+
+    try {
+      postService.deletePost(loginUser, postId);
+      return ResponseEntity.ok()
+          .body(new CommonResponseDto("삭제 완료", HttpStatus.OK.value()));
+    } catch (BusinessException be) {
+      return ResponseEntity.status(be.getStatus())
+          .body(new CommonResponseDto(be.getMessage(), be.getStatus()));
+    }
+  }
 }
--- src/main/java/com/plus/plus/post/service/PostService.java
+++ src/main/java/com/plus/plus/post/service/PostService.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +20,7 @@ public class PostService {
 
   private final PostRepository postRepository;
 
+  @Transactional
   public PostResponseDto createPost(User loginUser, PostRequestDto requestDto) {
     Post post = new Post(loginUser, requestDto);
     return new PostResponseDto(postRepository.save(post));
@@ -34,22 +36,33 @@ public class PostService {
         .stream().map(PostResponseDto::new).toList();
   }
 
+  @Transactional
   public PostResponseDto updatePost(
       PostUpdateRequestDto requestDto, User loginUser, Long postId) {
 
     Post post = getPost(postId);
-
-    // 로그인 한 유저와 작성자가 일치하는지 확인
-    if (!Objects.equals(loginUser.getUsername(), post.getAuthor())) {
-      throw new AuthenticationMismatchException();
-    }
-
-   post.update(requestDto);
+    getUser(loginUser, post);
+    post.update(requestDto);
     return new PostResponseDto(post);
   }
 
+  @Transactional
+  public void deletePost(User loginUser, Long postId) {
+
+    Post post = getPost(postId);
+    getUser(loginUser, post);
+    postRepository.delete(post);
+  }
+
   // postId에 해당하는 게시글을 가지고 오는 메서드
-  public Post getPost(Long postId) {
+  private Post getPost(Long postId) {
     return postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+  }
+
+  // 로그인 한 유저와 작성자가 일치하는지 확인하는 메서드
+  private void getUser(User loginUser, Post post) {
+    if (!Objects.equals(loginUser.getUsername(), post.getAuthor())) {
+      throw new AuthenticationMismatchException();
+    }
   }
 }
