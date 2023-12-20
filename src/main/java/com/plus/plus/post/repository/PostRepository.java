6d104263638d0496e596345c3617d891ef65a2d5
oneseel <seno1006@gmail.com>
2023-12-20 15:36:36 +0900

    feat : 게시글 삭제 스케줄러

--- src/main/java/com/plus/plus/post/repository/PostRepository.java
+++ src/main/java/com/plus/plus/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.plus.plus.post.repository;
 
 import com.plus.plus.post.entity.Post;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   List<Post> findAllByOrderByCreatedDateDesc(); // 만든날짜를 기준으로 내림차순
+
+  void deleteByModifiedDateBefore(LocalDateTime ninetyDaysAgo);
 }
--- src/main/java/com/plus/plus/user/entity/User.java
+++ src/main/java/com/plus/plus/user/entity/User.java
@@ -8,12 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
